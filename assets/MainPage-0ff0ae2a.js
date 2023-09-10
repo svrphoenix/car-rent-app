935596ba8557d652c4d52d0ee367695fad8ca000
Serhii Rudenko <svr.phoenix@gmail.com>
2023-09-10 09:44:13 +0300

    Updating imports

--- assets/MainPage-0ff0ae2a.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-38b8f0a1.js";const i=()=>t.jsx("div",{style:{height:"100vh",width:"100%"},children:"MainPage"});export{i as default};
---
+++ assets/MainPage-e57518a8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-60660e98.js";const i=()=>t.jsx("div",{style:{height:"100vh",width:"100%"},children:"MainPage"});export{i as default};
